fe56281c1ba7e26a6166590274301e84f6924389
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2024-11-07 21:40:49 +0600

    from office

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import Cart from '../Cart/Cart';
-import Wishlist from '../Wishlist/Wishlist';
 import { getStoredCart } from '../utility/cart';
+import { LiaDollarSignSolid } from "react-icons/lia";
 
 const Dashboard = () => {
     document.title = "Dashboard | Gadget Heaven";
@@ -36,20 +35,35 @@ const Dashboard = () => {
             <div className='h-[180px]'></div>
             <div>
                 <Tabs>
-                    <TabList>
+                    <TabList className={'mb-96'}>
                         <div className='text-center'>
                         <Tab>Cart</Tab>
                         <Tab>Wishlist</Tab>
                         </div>
                     </TabList>
 
-                    <TabPanel>
+                    <TabPanel className={'absolute min-w-[600px] bg-gray-100 top-96 w-10/12 left-7 lg:left-24 p-4 rounded-lg'}>
+                        <div>
                         <h2 className='text-2xl font-bold'>Cart{cartList.length}</h2>
-                        <Cart></Cart>
+                        {
+                            cartList.map(gadget => <div>
+                                <div className='flex bg-white mb-4'>
+                                <div className='w-56 h-36 flex justify-center py-2'>
+                                    <img src={gadget.product_image} alt={gadget.product_id} className='rounded-xl w-32 object-cover'/>
+                                </div>
+                                <div className='flex flex-col justify-evenly'>
+                                    <h2 className='font-bold text-xl'>{gadget.product_title}</h2>
+                                    <p className='text-gray-500'>{gadget.description}</p>
+                                    <p className='flex items-center content-center'>Price: <span className='text-md'><LiaDollarSignSolid /></span>{gadget.price}</p>
+                                </div>
+                                </div>
+                            </div>)
+                        }
+                        </div>
+                        
                     </TabPanel>
                     <TabPanel>
                         <h2 className='text-2xl font-bold'>Wishlist</h2>
-                        <Wishlist></Wishlist>
                     </TabPanel>
                 </Tabs>
             </div>
